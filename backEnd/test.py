c64b27210849528feebfd963f40158cc486d188c
onpk <nikraok@gmail.com>
2023-12-20 19:33:21 -0500

    Added the OpenAI information

--- backEnd/test.py
+++ backEnd/test.py
@@ -1,1 +1,28 @@
-print("test")
+'''import openai
+API_Key=''
+openai.api_key=API_Key
+
+model_id='gpt-3.5-turbo'
+vclient=openai.OpenAI()
+response=vclient.chat.completions.create(model=model_id, messages=[
+    {}
+])'''
+import openai
+import os
+key= "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"
+system = [{"role": "system", "content": "You are a helpful AI assistant."}]
+user = [{"role": "user", "content": "Introduce yourself."}]
+chat = []
+vclient=openai.OpenAI(api_key=key)
+while not user[0]['content'] == "exit":
+    response = vclient.chat.completions.create(
+        messages = system + chat[-10:] + user,
+        model="gpt-3.5-turbo", stream=True)
+    reply = ""
+    for delta in response:
+        if not delta['choices'][0]['finish_reason']:
+            word = delta['choices'][0]['delta']['content']
+            reply += word
+            print(word, end ="")
+    chat += user + [{"role": "assistant", "content": reply}]
+    user = [{"role": "user", "content": input("\nPrompt: ")}]
