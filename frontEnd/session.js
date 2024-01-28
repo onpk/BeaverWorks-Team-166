9baa5339480386d8895076af608716e939fbb5d2
SinghQuasar <shrerajsingh@outlook.com>
2024-01-28 17:02:04 -0500

    Changed session from hardcode to softcode

--- frontEnd/session.js
+++ frontEnd/session.js
@@ -1,36 +1,9 @@
-var nsch = 11;
-var nsum = 3;
-var nvg = 3;
-var nspt = 5;
-var nscsm = 3;
-var nt = 2;
+const cats = ["School", "Summer", "Friends", "Sports", "Sarcasm", "Texting"];
+const n = [8, 3, 2, 4, 3, 2];
+const t = [20, 5, 3, 6, 3, 3];
 
-var tsch = 20;
-var tsum = 5;
-var tvg = 3;
-var tspt = 6;
-var tscsm = 3;
-var tt = 3;
-
-console.log("Amount Remaining")
-document.getElementById("sch").textContent="School".concat(" (", nsch.toString(), ")");
-document.getElementById("sum").textContent="Summer".concat(" (", nsum.toString(), ")");
-document.getElementById("vg").textContent="Video Games".concat(" (", nvg.toString(), ")");
-document.getElementById("spt").textContent="Sports".concat(" (", nspt.toString(), ")");
-document.getElementById("scsm").textContent="Sarcasm".concat(" (", nscsm.toString(), ")");
-document.getElementById("t").textContent="Texting".concat(" (", nt.toString(), ")");
-
-var psch = document.getElementById("psch");
-var psum = document.getElementById("psum");
-var pvg = document.getElementById("pvg");
-var pspt = document.getElementById("pspts");
-var pscsm = document.getElementById("pscsm");
-var pt = document.getElementById("pt");
-
-console.log("Setting progress bar width");
-psch.style.width = ((tsch - nsch)/tsch)*100 + "px";
-psum.style.width = ((tsum - nsum)/tsum)*100 + "px";
-pvg.style.width = ((tvg - nvg)/tvg)*100 + "px";
-pspt.style.width = ((tspt - nspt)/tspt)*100 + "px";
-pscsm.style.width = ((tspt - nspt)/tspt)*100 + "px";
-pt.style.width = ((tt - nt)/tt)*100 + "px";
+for (let i = 0; i < cats.length; i++) {
+    document.getElementById("i" + (i+1).toString()).textContent=cats[i].concat(" (", n[i].toString(), ")");
+    var pn = document.getElementById("p" + (i+1).toString());
+    pn.style.width = ((t[i] - n[i])/t[i])*100 + "px"
+}
