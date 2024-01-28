b50553c8e5e71fd067bd3e25df54e54d272f9076
SinghQuasar <shrerajsingh@outlook.com>
2024-01-27 19:16:44 -0500

    created categories page showing categories left based on java script. beginning to implement progress bars

---
+++ frontEnd/session.js
@@ -0,0 +1,20 @@
+var nSchool = 11;
+var nSummer = 3;
+var nVideoGames = 3;
+var nSports = 5;
+var nSarcasm = 3;
+var nTexting = 2;
+
+var tsch = 20;
+var tsum = 5;
+var tvg = 3;
+var tspt = 6;
+var tscsm = 3;
+var tt = 3;
+
+document.getElementById("School").textContent="School".concat(" (", nSchool.toString(), ")");
+document.getElementById("Summer").textContent="Summer".concat(" (", nSummer.toString(), ")");
+document.getElementById("Video Games").textContent="Video Games".concat(" (", nVideoGames.toString(), ")");
+document.getElementById("Sports").textContent="Sports".concat(" (", nSports.toString(), ")");
+document.getElementById("Sarcasm").textContent="Sarcasm".concat(" (", nSarcasm.toString(), ")");
+document.getElementById("Texting").textContent="Texting".concat(" (", nTexting.toString(), ")");
